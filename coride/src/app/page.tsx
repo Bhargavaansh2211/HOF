38325aad72c9f6d991696e64d99f3fefe193a75a
Bhargavaansh2211 <143218592+Bhargavaansh2211@users.noreply.github.com>
2024-04-06 20:13:36 +0530

    added map

--- coride/src/app/page.tsx
+++ coride/src/app/page.tsx
@@ -1,15 +1,14 @@
-'use client'
+"use client"
 import dynamic from 'next/dynamic';
-import Link from "next/link"
-const Map = dynamic(() => import('../components/Map/Map'), {
-  ssr: false,
-});
 import { useEffect, useState } from 'react';
+import Link from 'next/link';
+import { GoogleMap, LoadScript, Autocomplete } from '@react-google-maps/api';
 
 interface AnimatedTextProps {
   text: string;
 }
-let t=0;
+
+let t = 0;
 
 const AnimatedText: React.FC<AnimatedTextProps> = ({ text }) => {
   const [displayedText, setDisplayedText] = useState('');
@@ -28,15 +27,14 @@ const AnimatedText: React.FC<AnimatedTextProps> = ({ text }) => {
     return () => clearInterval(interval);
   }, [text]);
   t++;
-  if (t==1){
+  if (t == 1) {
     return <span className="text-5xl font-bold block mt-36 ml-32">{displayedText}</span>;
-  }
-  else{
+  } else {
     return <span className="text-5xl font-bold block mt-8 ml-52">{displayedText}</span>;
   }
 };
 
-export default function Home() {
+const Home = () => {
   const [showWithCoRide, setShowWithCoRide] = useState(false);
 
   useEffect(() => {
@@ -48,30 +46,53 @@ export default function Home() {
   }, []);
 
   return (
-    <>
-       
+    
       <div className='flex w-full mt-10 '>
         <div className="relative w-[50%] ">
-        <AnimatedText text="AnyTime, AnyWhere" />
-        {showWithCoRide && (
-          <AnimatedText text="with CoRide" />
-        )}
-          <input 
-            type="text" 
-            placeholder="Enter Pick Up Point" 
-            className="border-2 border-black rounded-md mt-10 ml-52 px-4 h-14 w-80 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent" 
-          />
-          <input 
-            type="text" 
-            placeholder="Enter Destination Point" 
-            className="border-2 border-black rounded-md m-10 ml-52 px-4 h-14 w-80 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent" 
-          /><br/>
+          <AnimatedText text="AnyTime, AnyWhere" />
+          {showWithCoRide && (
+            <AnimatedText text="with CoRide" />
+          )}
+          <LoadScript
+            googleMapsApiKey="AIzaSyDq9wpSkS0FWn4KKqHQ8DHzXy6XuqkuyHw"
+            libraries={['places']}
+          >
+            <Autocomplete
+              onLoad={(autocomplete) => console.log('autocomplete: ', autocomplete)}
+              onPlaceChanged={() => console.log('onPlaceChanged')}
+            >
+              <input
+                type="text"
+                placeholder="Enter Pick Up Point"
+                className="border-2 border-black rounded-md mt-10 ml-52 px-4 h-14 w-80 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+              />
+            </Autocomplete>
+            <Autocomplete
+              onLoad={(autocomplete) => console.log('autocomplete: ', autocomplete)}
+              onPlaceChanged={() => console.log('onPlaceChanged')}
+            >
+              <input
+                type="text"
+                placeholder="Enter Destination Point"
+                className="border-2 border-black rounded-md m-10 ml-52 px-4 h-14 w-80 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+              />
+            </Autocomplete>
+            <GoogleMap
+              mapContainerStyle={{ height: '400px', width: '100%' }}
+              center={{ lat: -34.397, lng: 150.644 }}
+              zoom={8}
+            >
+              {/* Child components, such as markers, info windows, etc. */}
+              {/* Go here */}
+            </GoogleMap>
+            
+          </LoadScript>
+          <br />
           <Link href="/" className="text-black h-12 bg-yellow-400 ml-52 hover:bg-slate-400 hover:text-black rounded-lg p-2">See Prices</Link>
         </div>
-        <div className='w-[50%] mr-16 mt-12 h-max'>
-        <Map/>
-        </div>
       </div>
-    </>
+    
   );
 }
+
+export default Home;
