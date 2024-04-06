9481ccf417f2c4219d025ab8293b1a641f288c16
Bhargavaansh2211 <143218592+Bhargavaansh2211@users.noreply.github.com>
2024-04-06 17:23:46 +0530

    fixed onboarding bug

--- coride/src/app/sign-up/[[...sign-up]]/page.tsx
+++ coride/src/app/sign-up/[[...sign-up]]/page.tsx
@@ -1,9 +1,13 @@
+"use client"
 import { SignUp } from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
 
 export default function Page() {
+  const router = useRouter();
+
   return (
     <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
-      <SignUp />
+      <SignUp afterSignUpUrl={"/onboarding"} />
     </div>
   );
 }
